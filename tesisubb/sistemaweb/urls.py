3da6463584658166a6eef904c9ad287457f48c96
MSI\Sebastian <sebsaave@alumnos.ubiobio.cl>
2017-08-30 17:14:47 -0300

    Se  establece ruta de simular agregando un id para simular

--- tesisubb/sistemaweb/urls.py
+++ tesisubb/sistemaweb/urls.py
@@ -9,10 +9,8 @@ from . import views
 urlpatterns = [
     # url('/', views.index, name='index'),
     url('index', views.indexView, name='index'),
-
-    url ('upload', views.upload_file_view, name='upload_image_view'),
-    url ('(?P<pk>[0-9]+)/$', views.simularView),
-
+    url ('upload', views.cargar_archivo_simulacion_view, name='upload'),
+    url (r'^simular/(\d+)/$', views.simularView),
 ]
 
 
