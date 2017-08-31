62ba6bc69193478ee18ce282f29f1926757678b9
MSI\Sebastian <sebsaave@alumnos.ubiobio.cl>
2017-08-30 23:38:14 -0300

    Se envia id del request.POST del objeto Simular guardado a simular.html

--- tesisubb/sistemaweb/views.py
+++ tesisubb/sistemaweb/views.py
@@ -4,7 +4,7 @@ import os
 
 from django.core.files.storage import FileSystemStorage
 from django.http.response import HttpResponseRedirect
-from django.shortcuts import render, render_to_response, redirect
+from django.shortcuts import render, render_to_response, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.template.context import RequestContext
 from django.views.generic.edit import FormView
@@ -22,30 +22,30 @@ def indexView(request):
     return render (request, 'layout.html')
 
 
-def simularView(request):
-    return render (request, 'simular.html')
+def simularView(request, simulacion_id):
+    simulacion = get_object_or_404(Simular, pk=simulacion_id)
+    return render (request, 'simular.html', {'simulacion': simulacion})
 
 
 # HTTP Error 400
 def error_404View(request):
     return render (request, 'page_404.html')
 
-from .forms import UploadFileForm
+from .forms import CargarArchivosForm
 
 
-def upload_file_view(request):
+def cargar_archivo_simulacion_view(request):
 
     if request.method == 'POST':
-
-
-        form = UploadFileForm(request.POST, request.FILES)
-
+        form = CargarArchivosForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            a = form.auto_id
-            message = "Archivos cargados satisfactoriamente !"
-            return redirect('simular.html', pk=a)
+            nueva_simulacion = form.save()
+            simulacion =  get_object_or_404(Simular, pk=nueva_simulacion.pk )
+            # message = "Archivos cargados satisfactoriamente !"
+            return render (request, 'simular.html', {'simulacion': simulacion})
     else:
-        form = UploadFileForm()
+        form = CargarArchivosForm()
 
     return render(request, 'upload.html',locals(), {'form': form})
+
+
