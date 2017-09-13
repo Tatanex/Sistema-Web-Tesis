07173d675adebeda450d3d3349fbaf6898a7a3fd
MSI\Sebastian <sebsaave@alumnos.ubiobio.cl>
2017-09-13 14:38:19 -0300

    Se agrega ruta que permite ver reportes para cada simulaciòn en el servidor

--- tesisubb/sistemaweb/urls.py
+++ tesisubb/sistemaweb/urls.py
@@ -10,8 +10,11 @@ urlpatterns = [
     # url('/', views.index, name='index'),
     url('index', views.indexView, name='index'),
     url('upload', views.cargar_archivo_simulacion_view, name='upload'),
+    url('ver_simulaciones', views.ver_simulaciones, name='ver_simulaciones'),
     url(r'^simular/(\d+)/$', views.simularView, name='simular'),
-    url(r'^simular/(\d+)/submit$', views.submit, name='submit')
+    url(r'^simular/(\d+)/submit$', views.submit, name='submit'),
+    url(r'^ver/simulacion_(\d+)/$', views.ver, name='ver'),
+
 
 
 ]
