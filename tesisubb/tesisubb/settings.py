251313b08fdb7b5ceb3b9b7fc0a2ccee23052476
MSI\Sebastian <sebsaave@alumnos.ubiobio.cl>
2017-09-25 15:43:25 -0300

    Se eliminan espacios vacios en el codigo.

--- tesisubb/tesisubb/settings.py
+++ tesisubb/tesisubb/settings.py
@@ -130,9 +130,6 @@ SCRIPTSFILES_DIRS = (os.path.join(BASE_DIR,'scripts'),)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-
-
-
 # SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 # STATICFILES_DIRS = (os.path.join(SITE_ROOT, 'static/'),)
 # STATIC_URL = '/static/'
