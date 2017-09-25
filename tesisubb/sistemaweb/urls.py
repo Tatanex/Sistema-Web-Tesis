cf7d15a461f6ce9469723aa705edf9c6ad5026af
MSI\Sebastian <sebsaave@alumnos.ubiobio.cl>
2017-09-25 15:42:28 -0300

    Se agrega ruta 'comparar', compara archivos idfs. Se agrega ruta 'diferencia_comparacion', permite ver el resultado de la comparacion entre dos archivos idfs. Se agrega ruta 'detalle_objeto',permite ver los atributos y valores del objeto diferente.. Se agrega ruta 'modificar', permitira subir y modificar archivo idf.

--- tesisubb/sistemaweb/urls.py
+++ tesisubb/sistemaweb/urls.py
@@ -15,6 +15,19 @@ urlpatterns = [
     url(r'^simular/(\d+)/submit$', views.submit, name='submit'),
     url(r'^ver/simulacion_(\d+)/$', views.ver, name='ver'),
 
+    url('comparar', views.compararidf, name='comparar'),
+    url('diferencia_comparacion/(\d+)/$', views.diferencia_comparacion, name='diferencia_comparacion'),
+
+    # url ('detalle/objeto_objeto_(\d+)/$', views.ver_objeto, name='ver_objeto')
+    url ('detalle/(\d+)/(\w+)/(\w+)/$', views.ver_objeto, name='ver_objeto'),
+    url ('detalle/(\d+)/(\w+:\w+)/(\w+)/$', views.ver_objeto, name='ver_objeto'),
+    url ('ver_comparaciones', views.ver_comparaciones, name='ver_comparaciones'),
+    url ('ver_comparacion_(\d+)/$', views.ver_comparaciones, name='ver_comparaciones'),
+
+    url ('modificar', views.modificar, name='modificar'),
+
+
+
 
 
 ]
