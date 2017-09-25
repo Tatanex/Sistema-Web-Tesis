d715ba0fde3a0e7a4b039947406e21db49e0ba98
MSI\Sebastian <sebsaave@alumnos.ubiobio.cl>
2017-09-25 15:45:07 -0300

    Se agrega en Form clases para los modelos Comparar y Modificar.

--- tesisubb/sistemaweb/forms.py
+++ tesisubb/sistemaweb/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Simular
+from .models import Simular, Comparar
+
 
 class CargarArchivosForm(forms.ModelForm):
     class Meta:
@@ -7,3 +8,18 @@ class CargarArchivosForm(forms.ModelForm):
         fields = ['simular_id',
                   'simular_clima',
                   'simular_archivo_idf']
+
+
+class CompararIdfsForm(forms.ModelForm):
+    class Meta:
+        model = Comparar
+        fields = ['comparar_id',
+                  'comparar_archivo_idf1',
+                  'comparar_archivo_idf2']
+
+class ModificarIdfsForm(forms.ModelForm):
+    class Meta:
+        model = Comparar
+        fields = ['comparar_id',
+                  'comparar_archivo_idf1',
+                  'comparar_archivo_idf2']
