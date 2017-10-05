cd00ed81d34a1662d1c9c9003c6b07db0933f3c2
MSI\Sebastian <sebsaave@alumnos.ubiobio.cl>
2017-10-05 16:23:23 -0300

    Se agrega ruta de para el modulo modificar crear y ver_materials

--- tesisubb/sistemaweb/urls.py
+++ tesisubb/sistemaweb/urls.py
@@ -4,8 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
-
-
 urlpatterns = [
     # url('/', views.index, name='index'),
     url('index', views.indexView, name='index'),
@@ -16,20 +14,27 @@ urlpatterns = [
     url(r'^ver/simulacion_(\d+)/$', views.ver, name='ver'),
 
     url('comparar', views.compararidf, name='comparar'),
-    url('diferencia_comparacion/(\d+)/$', views.diferencia_comparacion, name='diferencia_comparacion'),
+    url('detalle_comparacion/(\d+)/$', views.detalle_comparacion, name='detalle_comparacion'),
+    url('detalle/(\d+)/(\w+)/(\w+)/$', views.ver_objeto, name='ver_objeto'),
+    url('detalle/(\d+)/(\w+:\w+)/(\w+)/$', views.ver_objeto, name='ver_objeto'),
+    url('ver_comparaciones', views.ver_comparaciones, name='ver_comparaciones'),
+    url('ver_comparacion_(\d+)/$', views.ver_comparaciones, name='ver_comparaciones'),
 
-    # url ('detalle/objeto_objeto_(\d+)/$', views.ver_objeto, name='ver_objeto')
-    url ('detalle/(\d+)/(\w+)/(\w+)/$', views.ver_objeto, name='ver_objeto'),
-    url ('detalle/(\d+)/(\w+:\w+)/(\w+)/$', views.ver_objeto, name='ver_objeto'),
-    url ('ver_comparaciones', views.ver_comparaciones, name='ver_comparaciones'),
-    url ('ver_comparacion_(\d+)/$', views.ver_comparaciones, name='ver_comparaciones'),
+    url('cargar_modificacion', views.cargar_modificacion, name='cargar_modificacion'),
+    url('modificar/(\d+)/(\w+)/(\w+)/$', views.modificar_objeto, name='modificar_objeto'),
+    # url('/(\d+)$', views.modificar_submit, name='modificar_submit'),
+    url('ver_modificaciones', views.ver_modificaciones, name='ver_modificaciones'),
+    url('modificar/(\d+)$', views.detalle_modificacion, name='detalle_modificacion'),
 
-    url ('modificar', views.modificar, name='modificar'),
+
+
+    url('crear_materials$', views.crear_materials, name='crear_materials'),
+    url('ver_materials$', views.ver_materials, name='ver_materials'),
+
+
 
 
 
 
 
 ]
-
-
