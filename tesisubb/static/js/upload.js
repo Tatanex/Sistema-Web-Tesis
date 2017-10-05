5a736bfcdc4b98f741a5f29b01a8c9436cc06b15
MSI\Sebastian <sebsaave@alumnos.ubiobio.cl>
2017-10-05 16:06:29 -0300

    Se agregan y modifican estilos y scripts para el funcionamiento en vistas del prototipo

---
+++ tesisubb/static/js/upload.js
@@ -0,0 +1,12 @@
+
+$("form").on("change", ".form-control", function() {
+  $(this)
+    .parent(".campo1, .campo2")
+    .attr(
+      "data-text",
+      $(this)
+        .val()
+        .replace(/.*(\/|\\)/, "")
+    );
+});
+
